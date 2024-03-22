2743edd711d8ac4c753ef173a831013a0a2dd583
Triet Ngo <triet.ngo@booking.com>
2024-03-22 15:59:47 +0100

    post

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { log } = require("console");
 
 dotenv.config();
 
@@ -9,6 +10,15 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=> {
     res.send("Hello there! Api is working")
+})
+
+app.post("/", (req,res)=> {
+    log("Request")
+    log(req)
+    log("Response")
+    log(res)
+    res.status(200)
+    res.end()
 })
 
 app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
