e34d69b1182c2c2cb09242cad07909de6cfdab2a
Triet Ngo <triet.ngo@booking.com>
2024-03-27 10:08:05 +0100

    a

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req,res)=> {
